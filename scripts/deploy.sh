5eb1941f92c6ff3f9d655e21dea5d28c7dfae954
bgpat <bgpat23@gmail.com>
2017-10-26 00:31:56 +0900

    Enable CI

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+#if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_BRANCH" != "gh-pages" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+	cd public
+
+	git config user.email "bot@kstm.shinshu-u.ac.jp"
+	git config user.name "kstm"
+
+	git add -A
+	git commit -m "Publish to gh-pages"
+
+	cd ..
+
+	git push "https://${GIT_TOKEN}@github.com/kstm-su/blog.git" gh-pages
+fi
+
