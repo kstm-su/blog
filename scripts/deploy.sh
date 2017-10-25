a1d31760fb9ee82386a9efc3f25f484bd6bcba7a
bgpat <bgpat23@gmail.com>
2017-10-26 02:25:45 +0900

    Fix permission

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,20 +3,4 @@
 set -eu
 set -o pipefail
 
-cd "$(dirname $0)/.."
-
-#if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-#if [ "$TRAVIS_BRANCH" != "gh-pages" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-	#cd public
-
-	#git config user.email "bot@kstm.shinshu-u.ac.jp"
-	#git config user.name "kstm"
-
-	#git add -A
-	#git commit -m "Publish to gh-pages"
-
-	#cd ..
-
-	#git push -f origin gh-pages
-#fi
-
+git push origin gh-pages
