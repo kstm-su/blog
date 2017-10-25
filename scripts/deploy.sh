6f39737ada17dfcfb25a83d3eabd0ba55be36dae
bgpat <bgpat23@gmail.com>
2017-10-26 01:54:04 +0900

    [WIP] Print debug messages on before deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,18 +6,6 @@ set -e
 if [ "$TRAVIS_BRANCH" != "gh-pages" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 	cd public
 
-	echo '$ pwd'
-	pwd
-
-	echo '$ ls'
-	ls -al *
-
-	echo '$ git status'
-	git status
-
-	echo '$ git worktree list'
-	git worktree list
-
 	git config user.email "bot@kstm.shinshu-u.ac.jp"
 	git config user.name "kstm"
 
